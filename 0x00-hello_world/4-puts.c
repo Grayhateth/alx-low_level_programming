87deefb7f926679f977637f93a9b71780c7ecbc7
grayhateth <ofiicialcybernetics@gmail.com>
2022-09-08 14:58:52 -0700

    task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,11 @@
  * Main - print the string in the put funciton
  *
  * description: using the main function
- * this program prints "programming is like building a multilingual puzzle"
+ * this program prints "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
